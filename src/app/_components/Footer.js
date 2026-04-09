eb695a5669780bd51ca91e142a68c4aeaaa8dcec
Pothe <keoutpothe2014@gmail.com>
2026-04-09 20:59:42 +0700

    Delete src directory

--- src/app/_components/Footer.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function Footer() {
-    return (
-        <footer className="border-t border-gray-400" >
-         <p className="max-w-screen-2xl mx-auto">copy right @the-wild-oasis 2026</p>
-       </footer>
-    )
-}
--- src/app/_components/Spinner.js
+++
@@ -1,9 +0,0 @@
-function Spinner() {
-  return (
-   <div className='flex items-center'>
-  <div className='w-36 h-36 border-4 border-gray-200 border-t-purple-500 rounded-full animate-spin'></div>
-</div>
-  )
-}
-
-export default Spinner;
