238e292277c3cab080f87a5e0109f9de0ccd7a3e
Pothe <keoutpothe2014@gmail.com>
2026-04-24 16:15:42 +0700

    login with google providers

--- the-wild-oasis-website-app/src/app/api/cabins/route.js
+++ the-wild-oasis-website-app/src/app/api/cabins/route.js
@@ -1,7 +1,10 @@
 import { getCabins } from "@/app/_lib/data-service"
 
-export async  function GET() {
-    const data = await getCabins()
-    return Response.json(data)
-    
-};
+export async function GET(){
+     try{
+        const Cabins = await getCabins()
+        return Response.json(Cabins)
+     }catch{
+        retu
+     }
+}
