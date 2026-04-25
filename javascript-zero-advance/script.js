250248a147575464a27409f22cfa6026aa209a27
Pothe <keoutpothe2014@gmail.com>
2026-04-25 09:54:01 +0700

    #

---
+++ javascript-zero-advance/script.js
@@ -0,0 +1,39 @@
+// for loop
+
+// for(let i=1;i<=20;i++){
+//     console.log(`លេខ​ ${i}` )
+// }
+
+// for(let i=10;i>=1; i--){
+//     console.log(i)
+// }
+
+
+// for(let i=1;i<=10;i++){
+//     for(let j=1;j<=10;j++){
+//         console.log(`let i ${i} let j${j} i+j${i+j}`)
+//     }
+// }
+// let st =1
+// while(st<=50){
+//     console.log(`student number ${st}`)
+//     st++
+// }
+
+// let count=0 
+// do{
+//     console.log(count)
+//     count++
+// }while(count<10)
+
+// let fruits =["bananas","apple","orange","mango"]
+const fruits ={a:"bananas",b:"orange"}
+
+
+// for(const fruit of fruits){
+//     console.log(fruit)
+// }
+
+for(const things in fruits){
+    console.log(things)
+}
